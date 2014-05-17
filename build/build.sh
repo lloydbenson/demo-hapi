0e2264c76179a87f820f080be04b97057b714a21
lloyd <lloyd@opensourceinfrastructure.com>
2014-05-17 10:49:54 -0500

    new repo

---
+++ build/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Building hapi demo"
+echo "Cleanup"
+rm -f hapidemo*.tar.gz
+rm -rf node_modules
+echo "Testing"
+npm install
+npm test
+rm -rf node_modules
+echo "Final install"
+npm install --production
+npm shrinkwrap
+VERSION=$(shasum npm-shrinkwrap.json | awk '{print $1}' | cut -c 1-7)
+tar cvfz demo-hapi-${VERSION}.tar.gz lib index.js package.json npm-shrinkwrap.json node_modules bin
---
+++ deploy/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Deploying App"
+echo "copying over app"
+exit 0
