f084d9fb6bff4eb0c88c534ea0cac0442b3f4e11
lloyd <lloyd@opensourceinfrastructure.com>
2014-05-17 11:34:47 -0500

    want a top level dir

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,7 @@ echo "Building hapi demo"
 echo "Cleanup"
 rm -f *.tar.gz
 rm -rf node_modules
+rm -rf demo-hapi
 echo "Testing"
 npm install
 npm test
@@ -12,4 +13,6 @@ echo "Final install"
 npm install --production
 npm shrinkwrap
 VERSION=$(sha1sum npm-shrinkwrap.json | awk '{print $1}' | cut -c 1-7)
-tar cvfz demo-hapi-${VERSION}.tar.gz lib index.js package.json npm-shrinkwrap.json node_modules bin
+mkdir demo-hapi
+mv lib index.js package.json npm-shrinkwrap.json node_modules bin demo-hapi
+tar cvfz demo-hapi-${VERSION}.tar.gz demo-hapi
