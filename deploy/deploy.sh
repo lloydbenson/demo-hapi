c0dc24446f117342d6cbb7184c84b30a489a4f85
lloyd <lloyd@opensourceinfrastructure.com>
2014-05-17 16:13:48 -0500

    fixing up deploy to be port

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DATE=$(date +%YYMMDDHHMM)
+PORT=8081
 echo "Download last successful artifact"
-ARTIFACT=$(curl -s "http://localhost:8080/job/demo.build/lastSuccessfulBuild/api/json?pretty=true" | grep fileName | awk '{print $3}' | awk -F\" '{print $2}')
-curl -s -L -O http://localhost:8080/job/demo.build/lastSuccessfulBuild/artifact/${ARTIFACT}
+ARTIFACT=$(curl -s "http://localhost:${PORT}/job/demo.build/lastSuccessfulBuild/api/json?pretty=true" | grep fileName | awk '{print $3}' | awk -F\" '{print $2}')
+curl -s -L -O http://localhost:${PORT}/job/demo.build/lastSuccessfulBuild/artifact/${ARTIFACT}
 echo "Copying ${ARTIFACT} for last successful run"
 echo "Cleaning up last backup"
 rm -rf prev-app
