381fad4177972f3941deab09c4f59f1fc846b9f8
lloyd <lloyd@opensourceinfrastructure.com>
2014-05-17 11:28:55 -0500

    do just a local test

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,16 +5,15 @@ echo "Download last successful artifact"
 ARTIFACT=$(curl -s "http://localhost:8080/job/demo.build/lastSuccessfulBuild/api/json?pretty=true" | grep fileName | awk '{print $3}' | awk -F\" '{print $2}')
 curl -s -L -O http://localhost:8080/job/demo.build/lastSuccessfulBuild/artifact/${ARTIFACT}
 echo "Copying ${ARTIFACT} for last successful run"
-cp ${BUILD_PATH}/${ARTIFACT} .
 echo "Cleaning up last backup"
 rm -rf prev-app-*
 echo "Stopping App"
-demo-hapi/bin/stop.sh
+app/demo-hapi/bin/stop.sh
 echo "Backup App"
 mv app prev-app 
 echo "Deploying App"
 mkdir app
 mv ${ARTIFACT} app
 tar xvfz ${ARTIFACT}
-demo-hapi/bin/start.sh
+app/demo-hapi/bin/start.sh
 exit 0
