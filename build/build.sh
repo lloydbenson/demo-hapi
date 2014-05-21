831bf11138ccb38c8953441c961933d26e7b4ed2
lloydbenson <lloyd.benson@gmail.com>
2014-05-21 07:41:48 -0500

    support mac build

--- build/build.sh
+++ build/build.sh
@@ -25,7 +25,14 @@ rm -rf node_modules
 echo "Final install"
 npm install --production
 npm shrinkwrap
-VERSION=$(sha1sum npm-shrinkwrap.json | awk '{print $1}' | cut -c 1-7)
+if [ $(uname -s) == "Darwin" ];
+then
+   CHECK_SUM="shasum"
+else
+   CHECK_SUM="sha1sum"
+fi
+
+VERSION=$(${CHECK_SUM} npm-shrinkwrap.json | awk '{print $1}' | cut -c 1-7)
 mkdir demo-hapi
 mv lib index.js package.json npm-shrinkwrap.json node_modules bin demo-hapi
 tar cvfz demo-hapi-${VERSION}.tar.gz demo-hapi
