cebccaa81d2e0faa159d3839082265dc5de2054e
lloydbenson <lloyd.benson@gmail.com>
2014-05-21 07:19:29 -0500

    just put it in the build

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+echo "Checking if npm is installed"
+## checking nvm version
+if [ -e ~/.nvm/nvm.sh ];
+then
+   source ~/.nvm/nvm.sh
+fi
+npm --version
+if [ $? -ne 0 ];
+then
+  echo "npm is not installed.  Try https://github.com/creationix/nvm"
+  exit 2
+fi
+
 echo "Building hapi demo"
 echo "Cleanup"
 rm -f *.tar.gz
