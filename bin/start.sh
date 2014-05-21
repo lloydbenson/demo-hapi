3c4cd735ac3e7195fe6e07eb4e135f6f6e4fb927
lloydbenson <lloyd.benson@gmail.com>
2014-05-21 07:32:58 -0500

    add here too

--- bin/start.sh
+++ bin/start.sh
@@ -1,1 +1,12 @@
+## checking node version
+if [ -e ~/.nvm/nvm.sh ];
+then
+   source ~/.nvm/nvm.sh
+fi
+node --version
+if [ $? -ne 0 ];
+then
+  echo "node is not installed.  Try https://github.com/creationix/nvm"
+  exit 2
+fi
 node index.js > demo-hapi.log 2>&1 &
